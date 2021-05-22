424148ede4b42b77989928e8952b8f584d922698
Tomasz Kowalski <bugen25@gmail.com>
2021-05-22 23:54:39 +0200

    Display rounded rate score

--- src/app/books/services/review.service.ts
+++ src/app/books/services/review.service.ts
@@ -1,31 +1,14 @@
-import { Injectable } from "@angular/core";
-import { AppHttpClient } from "src/app/app-common/app-http-client.service";
-import { FormReview } from "./review.model";
+import { Injectable } from '@angular/core';
+import { AppHttpClient } from 'src/app/app-common/app-http-client.service';
+import { FormReview } from './review.model';
 
-
-
-
-
-@Injectable({ providedIn: "root" })
+@Injectable({ providedIn: 'root' })
 export class ReviewService {
+  constructor(private httpClient: AppHttpClient) {}
 
-
-
-constructor(private httpClient: AppHttpClient) {}
-
-
-
-
-
-public addReview(review: FormReview) {
-  const reviewData = new FormData();
-  reviewData.append("content", review.content);
-  reviewData.append("rate", review.rate);
-  this.httpClient.post('/reviews', reviewData)
-    .subscribe();
-
-}
-
-
-
+  public addReview(review: FormReview) {
+    this.httpClient
+      .post('/reviews', { content: review.content, rate: review.rate })
+      .subscribe();
+  }
 }
